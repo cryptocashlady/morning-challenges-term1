5e87ec46531fc0f8181573c37815e35230b4d792
Holly <hollykingprojects@gmail.com>
2018-09-06 11:07:47 +1000

    sync master

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -2,11 +2,16 @@
 
 # The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
 
-  def fib_seq(num)
-   if num < 2
-      return [num]
-   else
-      fib_seq(num-1) + fib_seq(num-2)
-   end
+  #def fib_seq(num)
+   #if num < 2
+      #return [num]
+   #else
+      #fib_seq(num-1) + fib_seq(num-2)
+   #end
+ #end
+ #puts fib_seq(4)
+
+ def divide (dividend,divisor)
+    quotient = dividend/divisor)
  end
- puts fib_seq(4)
+    
